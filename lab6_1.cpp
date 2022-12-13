2d0aec0bf3178b69190203840bafb2c5b6792c3c
thanabordeee <jejekup00123@hotmail.co.th>
2022-12-13 15:53:25 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int i=1,j = 0,k = 0;
+    while(i!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> i;
+    if(i%2==0){
+    j++;
+    }
+    else
+    {
+    k++;
+    }
+    }
+    cout << "#Even numbers = "<< j-1 << '\n';
+    cout << "#Odd numbers = "<< k;
     return 0;
 }
